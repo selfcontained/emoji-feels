bfa00c0bbe1f32415beccebcfc6202e42752566f
Brad Harris <bmharris@gmail.com>
2015-07-31 14:18:12 -0600

    thingz

--- server/config/config.js
+++ server/config/config.js
@@ -26,10 +26,6 @@ module.exports = {
         },
         twitter: {
             enabled: true,
-            consumer_key: 'P2jERUxcHBleVPrrECziA',
-            consumer_secret: 'An16FloSoNJjHRhiw0BY7pCtlXrm7vhjnULJgPyU',
-            access_token_key: '14579804-LNbCkjQ4tzo3lhi2IFZe7A8E3Cqz3WuUE5YeGzfoo',
-            access_token_secret: 'Ernhy9nELMiUIycSFxz0n4mugVxynPHgviXAa3ouC4'
         },
         assets: {
             dynamic: true,
--- server/config/index.js
+++ server/config/index.js
@@ -5,6 +5,7 @@ module.exports = function(app) {
     var environment = process.env.NODE_ENV || 'development',
         crampon = new Crampon(['production', 'development'])
             .addFile(path.join(__dirname,'config.js'))
+            .addOverrideFile(path.join(app.ROOT, 'secrets.js'), true)
             .addOverride({ environment: environment });
 
     return crampon.getConfig(environment);
